4765d5dce25e15745df8b6ffaa7a287af6d61db7
harshitbisht96 <harshitbisht99@gmail.com>
2019-06-30 04:12:19 +0530

    Added bootstrap and made some UI changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageListComponent } from './image-list/image-list.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+
 
 
 @NgModule({
@@ -17,7 +20,10 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/image-list/image-list.component.ts
+++ src/app/image-list/image-list.component.ts
@@ -9,6 +9,7 @@ import { ImageService } from '../shared/image.service';
 export class ImageListComponent implements OnInit {
   images: any[];
   imagesFound:boolean=false;
+  searching: boolean = false;
 
   constructor( private _imageService : ImageService) { }
 
@@ -18,9 +19,26 @@ export class ImageListComponent implements OnInit {
     console.log(data.hits);
   }
 
+  handleError(error: any){
+    console.log(error);
+  }
+
+  setId(id: string,idx:number)
+  {
+    // console.log(idx);
+    // let elem=document.getElementById(id);
+    // console.log(elem.getAttribute("data-target"));
+    // elem.setAttribute("data-target","#"+idx.toString());
+  }
+
   searchImages(query:string)
   {
-    return this._imageService.getImage(query).subscribe( data=> this.handleSuccess(data) );
+    console.log(query);
+    this.searching= true;
+    console.log(this.searching);
+    return this._imageService.getImage(query).subscribe( data=> this.handleSuccess(data),
+    error =>this.handleError(error),
+    () => this.searching=false );
   }
 
   ngOnInit() {
